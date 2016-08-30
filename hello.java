72a71e13fb3eeb4ff54c29c7367bc723ec0c6df9
feedmo <simple_feedmo@hotmail.com>
2016-08-30 14:48:33 +0800

    change print.

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ public class hello
 	public static void main(String args[]){
 		System.out.println("hello world");
 		System.out.println("Are you ok? ");
-		System.out.println("yeah, I am fine. ");
+		
 	}
 }
